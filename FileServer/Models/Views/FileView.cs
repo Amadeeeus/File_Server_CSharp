b62102294fd9d7cd230c2aa465659f50a91937df
p.kravchenko <p.kravchenko@sh.dunice.net>
2024-10-21 12:08:09 +0300

    Create dev

--- FileServer/Models/Views/FileView.cs
+++
@@ -1,9 +0,0 @@
-namespace FileServer.Models.Views;
-
-public class FileView
-{
-    public string Id { get; set; }
-    public string FileName { get; set; }
-    public string ContentType { get; set; }
-    public string FilePath { get; set; }
-}
